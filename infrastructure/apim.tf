46060490b4feb3a7d2eede1c52a93e4b45b864e2
Dave Thompson <davethompson21@gmail.com>
2023-03-29 16:25:06 +0100

    - removing key value bits - aat.tfvars -> stg.tfvars

--- infrastructure/apim.tf
+++ infrastructure/apim.tf
@@ -1,32 +1,11 @@
 locals {
-  key_vault_name           = "${var.product}-${var.env}"
-  key_vault_resource_group = "${var.product}-service-${var.env}"
-  api_mgmt_name            = "${var.env}" == "aat" ? "cft-api-mgmt-stg" : "cft-api-mgmt-${var.env}"
-  api_mgmt_resource_group  = "cft-${var.env}-network-rg"
-  api_mgmt_product_name    = "${var.product}-${var.component}"
-  api_mgmt_api_name        = "${var.product}-${var.component}-api"
-  api_base_path            = var.product
-  url_darts_gateway        = "http://darts-gateway-${var.env}.service.core-compute-${var.env}.internal"
-  url_swagger              = "https://raw.githubusercontent.com/hmcts/darts-gateway/demo/src/main/resources/test.wsdl"
-
-  key_vault_subscription_id = {
-    stg = {
-      subscription = "74dacd4f-a248-45bb-a2f0-af700dc4cf68"
-    }
-    dev = {
-      subscription = "867a878b-cb68-4de5-9741-361ac9e178b6"
-    }
-    demo = {
-      subscription = "c68a4bed-4c3d-4956-af51-4ae164c1957c"
-    }
-  }
-}
-
-provider "azurerm" {
-  subscription_id            = local.key_vault_subscription_id["${var.env}"].subscription
-  skip_provider_registration = "true"
-  features {}
-  alias = "key_vault_darts"
+  api_mgmt_name           = "${var.env}" == "aat" ? "cft-api-mgmt-stg" : "cft-api-mgmt-${var.env}"
+  api_mgmt_resource_group = "cft-${var.env}-network-rg"
+  api_mgmt_product_name   = "${var.product}-${var.component}"
+  api_mgmt_api_name       = "${var.product}-${var.component}-api"
+  api_base_path           = var.product
+  url_darts_gateway       = "http://darts-gateway-${var.env}.service.core-compute-${var.env}.internal"
+  url_swagger             = "https://raw.githubusercontent.com/hmcts/darts-gateway/demo/src/main/resources/test.wsdl"
 }
 
 # Include CNP module for setting up an APIM product
@@ -78,44 +57,4 @@ data "azurerm_api_management" "cft-api-mgmt" {
   resource_group_name = local.api_mgmt_resource_group
 }
 
-data "azurerm_key_vault" "key_vault" {
-  name                = local.key_vault_name
-  resource_group_name = local.key_vault_resource_group
-  provider            = azurerm.key_vault_darts
-}
-
 data "azuread_client_config" "current" {}
-
-# Set key vault permissions
-resource "azurerm_key_vault_access_policy" "key_vault_permissions" {
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-
-  tenant_id = data.azuread_client_config.current.tenant_id
-  object_id = data.azuread_client_config.current.object_id
-
-  secret_permissions = [
-    "Get"
-  ]
-
-  certificate_permissions = [
-    "Get",
-    "List"
-  ]
-}
-
-# Assign get certificate permissions to APIM so APIM can access it
-resource "azurerm_key_vault_access_policy" "kv_apim_policy" {
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-
-  tenant_id = data.azuread_client_config.current.tenant_id
-  object_id = data.azurerm_api_management.cft-api-mgmt.identity.0.principal_id
-
-  secret_permissions = [
-    "Get"
-  ]
-
-  certificate_permissions = [
-    "Get",
-    "List"
-  ]
-}
