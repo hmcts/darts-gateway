80307628cfa395996f654c22c207755aca5a2b33
Gareth Morris <garethmorris@uk-ghkytf7gq7.lan>
2023-04-21 09:44:06 +0100

    initial soap ws setup

---
+++ src/main/java/uk/gov/hmcts/reform/darts/webservice/CaseEndpoint.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.darts.webservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import javax.xml.bind.*;
+
+import com.service.mojdarts.synapps.com.AddLogEntry;
+import com.service.mojdarts.synapps.com.AddLogEntryResponse;
+
+@Endpoint
+public class CaseEndpoint {
+
+    private static final String NAMESPACE_URI = "http://com.synapps.mojdarts.service.com";
+
+	@Autowired
+	public CaseEndpoint() {
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "addLogEntry")
+	@ResponsePayload
+	public AddLogEntryResponse addLogEntry(@RequestPayload JAXBElement<AddLogEntry> request) {
+		AddLogEntryResponse response = new AddLogEntryResponse();
+        return response;
+	}
+
+}
