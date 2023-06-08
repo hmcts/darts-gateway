4b39cb1b5b1a9b4ac50260e8ffca745a42941d8a
tonym <incalabsltd@gmail.com>
2023-06-08 10:28:22 +0100

    DMP-228: Fixing duplicate module names.

--- infrastructure/apimspike.tf
+++ infrastructure/apimspike.tf
@@ -1,7 +1,7 @@
 # Include CNP module for setting up an API on an APIM product
 # Uses output variable from api_mgmt_product to set product_id
 # content_format needs to be set to wsdl-link as specs are in WSDL format
-module "api_mgmt_api" {
+module "api_mgmt_api_spike" {
   source         = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
   name           = "${local.api_mgmt_api_name}-spike"
   display_name   = "Darts Gateway API - SPIKE"
@@ -20,11 +20,11 @@ module "api_mgmt_api" {
 
 # Include CNP module for setting up a policy on an API
 # Uses output variable from api_mgmt_api to set api_name
-module "api-mgmt-api-policy" {
+module "api-mgmt-api-spike-policy" {
   source                 = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
   api_mgmt_name          = local.api_mgmt_name
   api_mgmt_rg            = local.api_mgmt_resource_group
-  api_name               = module.api_mgmt_api.name
+  api_name               = module.api_mgmt_api_spike.name
   api_policy_xml_content = file("${path.module}/apim-policy/api-spike-policy.xml")
   providers = {
     azurerm = azurerm.aks-sdsapps
