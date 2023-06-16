1d584c8b45177ebafea2481d7e6bf2d0f28c7de5
tonym <incalabsltd@gmail.com>
2023-06-16 15:08:06 +0100

    Adding Unit test coverage for response utils

---
+++ src/test/java/uk/gov/hmcts/darts/ws/DartsResponseUtilsTest.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.darts.ws;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.darts.exceptions.DartsValidationException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DartsResponseUtilsTest {
+
+    private final DartsResponseUtils dartsResponseUtils = new DartsResponseUtils();
+
+    @Test
+    void createsDartsResponseFromCodeAndMessage() {
+        var dartsResponse = dartsResponseUtils.createResponseMessage(CodeAndMessage.OK);
+
+        assertThat(dartsResponse.getCode()).isEqualTo("200");
+        assertThat(dartsResponse.getMessage()).isEqualTo("OK");
+    }
+
+    @Test
+    void createsDartsResponseFromDartsValidationException() {
+        var validationException = new DartsValidationException(new RuntimeException());
+        var dartsResponse = dartsResponseUtils.createResponseMessage(validationException);
+
+        assertThat(dartsResponse.getCode()).isEqualTo("400");
+        assertThat(dartsResponse.getMessage()).isEqualTo("Invalid XML Document");
+    }
+
+    @Test
+    void defaultsTo500ForUnknownException() {
+        var dartsResponse = dartsResponseUtils.createResponseMessage(new RuntimeException());
+
+        assertThat(dartsResponse.getCode()).isEqualTo("500");
+        assertThat(dartsResponse.getMessage()).isNull();
+    }
+}
