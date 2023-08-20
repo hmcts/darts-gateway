b09858db506b58dbdebd58b5b2592a8fd0c42ee7
Chris Magowan <chris.magowan@hmcts.net>
2023-08-20 11:54:23 +0100

    timezone testing

--- src/main/java/uk/gov/hmcts/darts/utilities/DateUtil.java
+++ src/main/java/uk/gov/hmcts/darts/utilities/DateUtil.java
@@ -2,7 +2,10 @@ package uk.gov.hmcts.darts.utilities;
 
 import lombok.experimental.UtilityClass;
 
+import java.time.Instant;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.GregorianCalendar;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -10,6 +13,15 @@ import javax.xml.datatype.XMLGregorianCalendar;
 @UtilityClass
 public class DateUtil {
     public OffsetDateTime toOffsetDateTime(XMLGregorianCalendar date) {
+        GregorianCalendar gregorianCalendar = date.toGregorianCalendar();
+        ZonedDateTime zonedDateTime = gregorianCalendar.toZonedDateTime();
+        Instant instant = zonedDateTime.toInstant();
+        ZoneId zone = ZoneId.of("Europe/London");
+        ZoneOffset zoneOffSet = zone.getRules().getOffset(instant);
+        return instant.atOffset(zoneOffSet);
+    }
+
+    public OffsetDateTime toOffsetDateTime2(XMLGregorianCalendar date) {
         GregorianCalendar gregorianCalendar = date.toGregorianCalendar();
         ZonedDateTime zonedDateTime = gregorianCalendar.toZonedDateTime();
         return zonedDateTime.toOffsetDateTime();
