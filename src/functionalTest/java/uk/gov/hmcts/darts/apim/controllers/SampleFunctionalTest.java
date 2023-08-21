6775dfcbf87db6099479efad0e0288a643a6746e
tonym <incalabsltd@gmail.com>
2023-08-21 19:08:17 +0100

    Added local pact broker

--- src/functionalTest/java/uk/gov/hmcts/darts/apim/controllers/SampleFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/darts/apim/controllers/SampleFunctionalTest.java
@@ -13,6 +13,7 @@ import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SampleFunctionalTest {
+
     protected static final String CONTENT_TYPE_VALUE = "application/json";
 
     @Value("${TEST_URL:http://localhost:8080}")
