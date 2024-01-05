b439eeb93642474ec6f1a02c0f6ad02cb31006ed
mmmalik1 <mohammed.malik1@hmcts.net>
2024-01-05 16:27:31 +0000

    json_document updated to json_string

--- src/main/java/uk/gov/hmcts/darts/dailylist/DailyListRoute.java
+++ src/main/java/uk/gov/hmcts/darts/dailylist/DailyListRoute.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.darts.dailylist;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.synapps.moj.dfs.response.DARTSResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Service;
 import uk.gov.courtservice.schemas.courtservice.DailyListStructure;
 import uk.gov.hmcts.darts.common.client.DailyListsClient;
 import uk.gov.hmcts.darts.common.exceptions.DartsValidationException;
+import uk.gov.hmcts.darts.config.ServiceConfig;
 import uk.gov.hmcts.darts.dailylist.enums.SystemType;
 import uk.gov.hmcts.darts.dailylist.mapper.DailyListRequestMapper;
 import uk.gov.hmcts.darts.dailylist.mapper.DailyListXmlRequestMapper;
 import uk.gov.hmcts.darts.dailylist.model.PostDailyListRequest;
+import uk.gov.hmcts.darts.model.dailylist.DailyListJsonObject;
 import uk.gov.hmcts.darts.model.dailylist.PostDailyListResponse;
 import uk.gov.hmcts.darts.utilities.XmlParser;
 import uk.gov.hmcts.darts.utilities.XmlValidator;
@@ -55,25 +58,21 @@ public class DailyListRoute {
 
         DailyListStructure legacyDailyListObject = xmlParser.unmarshal(document.trim(), DailyListStructure.class);
 
-        //TODO: Need to change the specification to take a string and then uncomment this section
-        /*
         DailyListJsonObject modernisedDailyList = dailyListRequestMapper.mapToEntity(legacyDailyListObject);
 
         String modernisedDailyListJson;
         try {
             modernisedDailyListJson = ServiceConfig.getServiceObjectMapper().writeValueAsString(modernisedDailyList);
-
-            //TODO: Need to validate against the Json schema here when it is split from the
-            // openapispec
         } catch (JsonProcessingException ex) {
             throw new DartsException(ex, CodeAndMessage.INVALID_XML);
         }
-        */
+
 
         PostDailyListRequest postDailyListRequest = DailyListXmlRequestMapper.mapToPostDailyListRequest(
                 legacyDailyListObject,
                 document
         );
+
         ResponseEntity<PostDailyListResponse> postDailyListResponse = dailyListsClient.dailylistsPost(
             systemType.get().getModernisedSystemType(),
             postDailyListRequest.getCourthouse(),
@@ -81,19 +80,14 @@ public class DailyListRoute {
             postDailyListRequest.getUniqueId(),
             OffsetDateTimeTypeDeserializer.getLOffsetDate(postDailyListRequest.getPublishedTs()),
             postDailyListRequest.getDailyListXml(),
-
-            //TODO: Need to change the specification to take a string and then pass modernisedDailyListJson. Feign will
-            // not work with objects passed as headers
-            null
+            modernisedDailyListJson
         );
 
         Integer dalId = postDailyListResponse.getBody().getDalId();
-        // TODO: We need to ensure we pass a string for the json as for the reasons below
+
         dailyListsClient.dailylistsPatch(
                 dalId,
-                //TODO: Need to change the specification to take a string and then pass modernisedDailyListJson. Feign will
-                // not work with objects passed as headers
-                null
+                modernisedDailyListJson
         );
         return successResponse();
     }
