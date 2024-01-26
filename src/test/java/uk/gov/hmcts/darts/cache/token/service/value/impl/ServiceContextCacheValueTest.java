1a0993adf692a368f61e81f850b343c826ec2690
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-01-26 18:54:18 +0000

    Checkstyle fix

--- src/test/java/uk/gov/hmcts/darts/cache/token/service/value/impl/ServiceContextCacheValueTest.java
+++ src/test/java/uk/gov/hmcts/darts/cache/token/service/value/impl/ServiceContextCacheValueTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.xml.transform.StringSource;
-import uk.gov.hmcts.darts.cache.token.component.TokenGenerator;
 import uk.gov.hmcts.darts.cache.token.service.TokenRegisterable;
 
 class ServiceContextCacheValueTest {
