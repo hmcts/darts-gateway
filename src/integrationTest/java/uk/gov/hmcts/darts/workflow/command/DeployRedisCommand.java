c07eb0f5089025eac79bdbf221925568cdb0d34e
Mr Michael Estebanez <63051024+mestebanez@users.noreply.github.com>
2024-02-23 17:37:44 +0000

    Dmp 2408 - gateway tests use testcontainer (#278)

---
+++ src/integrationTest/java/uk/gov/hmcts/darts/workflow/command/DeployRedisCommand.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.darts.workflow.command;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+
+import java.io.IOException;
+
+public class DeployRedisCommand implements Command {
+
+    private static GenericContainer<?> container;
+
+    static {
+        ImageFromDockerfile redisImage = new ImageFromDockerfile()
+            .withDockerfileFromBuilder(builder ->
+                                           builder
+                                               .from("redis:7.2.4-alpine")
+                                               .build());
+
+        container = new GenericContainer<>(redisImage);
+        container = container.withExposedPorts(6379);
+
+        container.start();
+        System.setProperty("darts-gateway.redis.connection-string", "redis://localhost:" + container.getMappedPort(6379).toString());
+        System.setProperty("darts-gateway.redis.ssl-enabled", "false");
+    }
+
+    @Override
+    public void cleanupResources() {
+        container.stop();
+    }
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void executeWithDocker() throws IOException {
+
+        if (!container.isRunning()) {
+            container.start();
+        }
+    }
+
+    @Override
+    public boolean isSuccess() {
+        return container.isRunning();
+    }
+}
