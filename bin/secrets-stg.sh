ae934e6de0e35d063e7ea275a63c699beba75059
john.race@hmcts.net <john.race@hmcts.net>
2024-02-26 11:10:13 +0000

    DMP-2232 Updated resources with AAD_B2C_CLIENT_ID_KEY

--- bin/secrets-stg.sh
+++ bin/secrets-stg.sh
@@ -8,6 +8,7 @@ export AAD_B2C_TENANT_ID_KEY="$(az keyvault secret show --vault-name darts-stg -
 export REDIS_CONNECTION_STRING="$(az keyvault secret show --vault-name darts-stg --name redis-connection-string  | jq .value -r)"
 export MAX_FILE_UPLOAD_SIZE_MEGABYTES="$(az keyvault secret show --vault-name darts-stg --name MaxFileUploadSizeInMegabytes | jq .value -r)"
 export AAD_B2C_ROPC_CLIENT_ID_KEY="$(az keyvault secret show --vault-name darts-stg --name AzureAdB2CFuncTestROPCClientId | jq .value -r)"
+export AAD_B2C_CLIENT_ID_KEY="$(az keyvault secret show --vault-name darts-stg --name AzureAdB2CClientId | jq .value -r)"
 export DAR_NOTIFY_EVENT_SECUREMENT_PASSWORD="$(az keyvault secret show --vault-name darts-stg --name darts-gateway-DarNotifyEventSecurementPassword | jq .value -r)"
 export DAR_NOTIFY_EVENT_SECUREMENT_USERNAME="$(az keyvault secret show --vault-name darts-stg --name darts-gateway-DarNotifyEventSecurementUsername   | jq .value -r)"
 export VIQ_EXTERNAL_USER_NAME="$(az keyvault secret show --vault-name darts-stg --name ViQExternalUserName | jq .value -r)"
