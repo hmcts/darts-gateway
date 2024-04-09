7a968cb93a956d1015e24473d23a0cb4b3b4d5ab
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-04-09 08:30:50 +0100

    Correct test assertion

--- src/integrationTest/java/uk/gov/hmcts/darts/workflow/AddAudioMtomMidTierWorkflowTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/workflow/AddAudioMtomMidTierWorkflowTest.java
@@ -70,7 +70,7 @@ class AddAudioMtomMidTierWorkflowTest extends AbstractWorkflowCommand {
                 .withRequestBody(new MultipartDartsProxyContentPattern()));
 
         Mockito.verify(validator, times(2)).test(Mockito.eq(Token.TokenExpiryEnum.DO_NOT_APPLY_EARLY_TOKEN_EXPIRY), Mockito.anyString());
-        Mockito.verify(validator, times(4)).test(Mockito.eq(Token.TokenExpiryEnum.APPLY_EARLY_TOKEN_EXPIRY), Mockito.anyString());
+        Mockito.verify(validator, times(3)).test(Mockito.eq(Token.TokenExpiryEnum.APPLY_EARLY_TOKEN_EXPIRY), Mockito.anyString());
 
         verify(postRequestedFor(urlPathEqualTo("/audios"))
                             .withHeader("Authorization", new RegexPattern("Bearer test")));
