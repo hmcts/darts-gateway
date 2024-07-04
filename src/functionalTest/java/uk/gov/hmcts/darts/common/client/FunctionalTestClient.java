0d19ae013abca07698a028046e28f204d7c325cf
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-07-04 18:09:37 +0100

    Re add clear down endpoint

--- src/functionalTest/java/uk/gov/hmcts/darts/common/client/FunctionalTestClient.java
+++ src/functionalTest/java/uk/gov/hmcts/darts/common/client/FunctionalTestClient.java
@@ -18,6 +18,18 @@ public class FunctionalTestClient {
     private final String baseLocation;
 
     public void clear() throws IOException, InterruptedException {
-       // DO nothing
+        HttpClient httpClient = HttpClient.newHttpClient();
+        
+        HttpRequest httpRequest = HttpRequest
+            .newBuilder()
+            .DELETE().uri(URI.create(baseLocation + "/functional-tests/clean"))
+            .build();
+
+
+        HttpResponse<String> httpResponses = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        if (HttpStatus.valueOf(httpResponses.statusCode()).is4xxClientError()
+            || HttpStatus.valueOf(httpResponses.statusCode()).is5xxServerError()) {
+            throw new AssertionError("Expected a successful clean down");
+        }
     }
 }
