759a436aa620c53d13cd06f983193c66aef86e17
mfesteba@yahoo.co.uk <63051024+mestebanez@users.noreply.github.com>
2024-07-04 12:29:01 +0100

    Small correction

--- src/main/java/uk/gov/hmcts/darts/common/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/darts/common/controllers/RootController.java
@@ -4,8 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-
 import static org.springframework.http.ResponseEntity.ok;
 
 /**
