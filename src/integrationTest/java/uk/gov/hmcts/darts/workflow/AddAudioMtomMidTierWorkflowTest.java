c6e27f9f310b46c132a8cba83581bcf370e45bb6
Mr Michael Estebanez <63051024+mestebanez@users.noreply.github.com>
2024-07-17 12:57:34 +0100

    Renable mtom test using documentum client (#478)

--- src/integrationTest/java/uk/gov/hmcts/darts/workflow/AddAudioMtomMidTierWorkflowTest.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/workflow/AddAudioMtomMidTierWorkflowTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.darts.workflow;
 import com.github.tomakehurst.wiremock.matching.RegexPattern;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -30,7 +29,6 @@ import static org.mockito.Mockito.when;
 
 @ActiveProfiles("int-test-jwt-token")
 @org.testcontainers.junit.jupiter.Testcontainers(disabledWithoutDocker = true)
-@Disabled
 class AddAudioMtomMidTierWorkflowTest extends AbstractWorkflowCommand {
     @MockBean
     private TokenGenerator generator;
