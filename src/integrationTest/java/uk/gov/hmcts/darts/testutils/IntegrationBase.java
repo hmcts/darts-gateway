f0d3d4bf0ebf8480bb727d545b7185ded3c7e477
benedwards <ben.edwards@cgi.com>
2024-11-21 12:17:44 +0000

    Fixed styles

--- src/integrationTest/java/uk/gov/hmcts/darts/testutils/IntegrationBase.java
+++ src/integrationTest/java/uk/gov/hmcts/darts/testutils/IntegrationBase.java
@@ -28,7 +28,6 @@ import uk.gov.hmcts.darts.testutils.stub.GetCasesApiStub;
 import uk.gov.hmcts.darts.testutils.stub.GetCourtLogsApiStub;
 import uk.gov.hmcts.darts.testutils.stub.PostCourtLogsApiStub;
 import uk.gov.hmcts.darts.testutils.stub.TokenStub;
-import uk.gov.hmcts.darts.utilities.XmlParser;
 import uk.gov.hmcts.darts.workflow.command.Command;
 import uk.gov.hmcts.darts.workflow.command.CommandHolder;
 import uk.gov.hmcts.darts.workflow.command.DeployRedisCommand;
