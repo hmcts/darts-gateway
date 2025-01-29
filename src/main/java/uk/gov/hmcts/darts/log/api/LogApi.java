dce42eb6e08c51048f62bd9927efeb6d0d2ca575
benedwards <ben.edwards@cgi.com>
2025-01-29 10:47:37 +0000

    Added additional error logs

--- src/main/java/uk/gov/hmcts/darts/log/api/LogApi.java
+++ src/main/java/uk/gov/hmcts/darts/log/api/LogApi.java
@@ -1,38 +1,46 @@
 package uk.gov.hmcts.darts.log.api;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.slf4j.event.Level;
 
 import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @SuppressWarnings("PMD.UseObjectForClearerAPI")
 public interface LogApi {
 
     void notificationSucceeded(
-            String uri,
-            String courthouse,
-            String courtroom,
-            String caseNumber,
-            OffsetDateTime offsetDateTime,
-            int responseCode);
+        String uri,
+        String courthouse,
+        String courtroom,
+        String caseNumber,
+        OffsetDateTime offsetDateTime,
+        int responseCode);
 
     void notificationFailed(
-            String uri,
-            String courthouse,
-            String courtroom,
-            String caseNumber,
-            OffsetDateTime offsetDateTime,
-            String status,
-            String message,
-            Level logLevel);
+        String uri,
+        String courthouse,
+        String courtroom,
+        String caseNumber,
+        OffsetDateTime offsetDateTime,
+        String status,
+        String message,
+        Level logLevel);
 
     void notificationFailedWithCode(
-            String uri,
-            String courthouse,
-            String courtroom,
-            String caseNumber,
-            OffsetDateTime offsetDateTime,
-            String status,
-            String message,
-            int code,
-            Level logLevel);
+        String uri,
+        String courthouse,
+        String courtroom,
+        String caseNumber,
+        OffsetDateTime offsetDateTime,
+        String status,
+        String message,
+        int code,
+        Level logLevel);
+
+    void failedToLinkAudioToCases(String courthouse, String courtroom, OffsetDateTime startedAt, OffsetDateTime endedAt, List<String> cases,
+                                  String checksum, UUID storageBlobId);
 }
