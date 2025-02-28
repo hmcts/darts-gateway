766a88e7c9bbf0732229657111c7d0a3488869bb
benedwards <ben.edwards@cgi.com>
2025-02-28 11:18:16 +0000

    Updated error logging

--- src/main/java/uk/gov/hmcts/darts/common/client/exeption/JacksonFeignClientProblemDecoder.java
+++ src/main/java/uk/gov/hmcts/darts/common/client/exeption/JacksonFeignClientProblemDecoder.java
@@ -7,9 +7,13 @@ import uk.gov.hmcts.darts.common.client.mapper.APIProblemResponseMapper;
 import uk.gov.hmcts.darts.config.ServiceConfig;
 import uk.gov.hmcts.darts.model.audio.Problem;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class JacksonFeignClientProblemDecoder extends AbstractClientProblemDecoder implements ErrorDecoder {
@@ -20,9 +24,15 @@ public class JacksonFeignClientProblemDecoder extends AbstractClientProblemDecod
 
     @Override
     protected Problem getProblem(InputStream response) throws IOException {
+        log.error("A problem occurred when communicating downstream {}", toString(response));
         ObjectMapper mapper = ServiceConfig.getServiceObjectMapper();
-        Problem problem = mapper.readValue(response, Problem.class);
-        log.error("A problem occurred when communicating downstream {}", problem.toString());
-        return problem;
+        return mapper.readValue(response, Problem.class);
+    }
+
+    private String toString(InputStream inputStream) {
+        return new BufferedReader(
+            new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+            .lines()
+            .collect(Collectors.joining("\n"));
     }
 }
