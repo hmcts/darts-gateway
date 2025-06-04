1289e1ec532ea451bb89e74f1f624922d5adf695
benedwards <ben.edwards@cgi.com>
2025-06-04 15:41:42 +0100

    Updated tests

--- src/test/java/uk/gov/hmcts/darts/ws/DartsEndpointHandlerTest.java
+++ src/test/java/uk/gov/hmcts/darts/ws/DartsEndpointHandlerTest.java
@@ -2,26 +2,40 @@ package uk.gov.hmcts.darts.ws;
 
 import com.synapps.moj.dfs.response.DARTSResponse;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import uk.gov.hmcts.darts.common.exceptions.DartsException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DartsEndpointHandlerTest {
+    ;
 
-    private static final CodeAndMessage ASSERT_MESSAGE = CodeAndMessage.NOT_FOUND_COURTHOUSE;
-
-    @Test
-    void testEndpointExecution() {
+    @ParameterizedTest
+    @EnumSource(value = CodeAndMessage.class, names = "ERROR", mode = EnumSource.Mode.INCLUDE)
+    void testEndpointExecution_whenUsingACodeANdMessageWIthIsSuccessFalse_shouldThrowError(CodeAndMessage codeAndMessage) {
         DartsException exception = assertThrows(DartsException.class, () -> new DartsEndpointHandler().makeAPICall(
             "",
-            this::executeWithCourtHouseError,
+            () -> executeWithCourtHouseError(codeAndMessage),
             DARTSResponse::new
         ));
-        assertThat(exception.getCodeAndMessage()).isEqualTo(ASSERT_MESSAGE);
+        assertThat(exception.getCodeAndMessage()).isEqualTo(codeAndMessage);
     }
 
-    private DARTSResponse executeWithCourtHouseError() {
-        throw new DartsException(ASSERT_MESSAGE);
+    @ParameterizedTest
+    @EnumSource(value = CodeAndMessage.class, names = "ERROR", mode = EnumSource.Mode.EXCLUDE)
+    void testEndpointExecution_whenUsingACodeANdMessageWIthIsSuccessTrue_shouldNotThrowErrorButInsteadMapResponseFields(CodeAndMessage codeAndMessage) {
+        DARTSResponse response = new DartsEndpointHandler().makeAPICall(
+            "",
+            () -> executeWithCourtHouseError(codeAndMessage),
+            DARTSResponse::new
+        );
+        assertThat(response.getCode()).isEqualTo(codeAndMessage.getCode());
+        assertThat(response.getMessage()).isEqualTo(codeAndMessage.getMessage());
+    }
+
+    private DARTSResponse executeWithCourtHouseError(CodeAndMessage codeAndMessage) {
+        throw new DartsException(codeAndMessage);
     }
 }
